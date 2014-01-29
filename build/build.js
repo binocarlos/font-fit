95634c368e9b30b85c0d157a699fb18aa8bdcbf6
shennan <jackotisbarker@gmail.com>
2014-01-29 00:20:41 +0000

    More intelligent narrowing of size-stepping Hijacks line-height temporarily in order to avoid strange behaviour in legacy browsers

--- build/build.js
+++ build/build.js
@@ -375,8 +375,12 @@ module.exports = function(el, config){
 		// emify our element
 		emify(el);
 
-		// emulate the width of our container without text nodes
+		// remember the line-height value
+		var line_height = el.style.lineHeight;
+
+		// emulate the width of our container without text nodes and strange line-heights
 		el.style.fontSize = '0em';
+		el.style.lineHeight = 'normal';
 
 		// get the width and height of the containing element with font-size at 0
 		var w = el.offsetWidth;
@@ -385,7 +389,7 @@ module.exports = function(el, config){
 		// define a starting size and step amount
 		var size = step = max_size;
 
-		// declare a variable
+		// define an overflowing flag
 		var overflowing = true;
 
 		// keep an eye on how many times our while loop is iterating
@@ -401,8 +405,27 @@ module.exports = function(el, config){
 		  var overflow_w = (w < el.scrollWidth);
 		  var overflow_h = (h < el.scrollHeight);
 
-		  // formulate a condition based on the fit_width/fit_height variable flags
-		  overflowing = (fit_width && overflow_w) || (fit_height && overflow_h);
+		  // define a flag to indicate that we need to multiply our step
+		  var multiply;
+
+		  // if we're overflowing (based on a condition using the fit_width/fit_height variable flags and offset vs scroll)
+		  if((fit_width && overflow_w) || (fit_height && overflow_h)){
+
+		  	// adjust our multiply flag based on our previous overflowing flag
+		  	multiply = overflowing ? false : true;
+
+		  	// adjust our overflow flag
+		  	overflowing = true;
+
+		  }else{
+
+		  	// adjust our multiply flag based on our previous overflowing flag
+		  	multiply = overflowing ? true : false;
+
+		  	// adjust our overflow flag
+		  	overflowing = false;
+
+		  }
 		  
 		  // adjust our size according to our current step amount
 			size = (overflowing) ? size - step : size + step;
@@ -414,13 +437,22 @@ module.exports = function(el, config){
 
 			}
 
-			// narrow the range of our step
-			step *= multiplier;
+			// if we need to narrow our step
+			if(multiply){
+				
+				// narrow the range of our step
+				step *= multiplier;
+
+			}
 
 			// escape clause
 			if(i >= 100){ break }else{ i++; }
 
 		}
+
+		// return our line-height to its original value
+		el.style.lineHeight = line_height;
+
 	}
 
 	/* Note: padding and border support only for browsers with getComputedStyle */
@@ -434,7 +466,7 @@ module.exports = function(el, config){
 
 		// if the element has bottom padding/border
 		if(padding || border){
-
+			
 			// get the pixel/em ratio from the computed font-size of the parent element
 			var px_to_ems = (el.parentNode) ? parseFloat(document.defaultView.getComputedStyle(el.parentNode).getPropertyValue('font-size')) : 16;
 			
